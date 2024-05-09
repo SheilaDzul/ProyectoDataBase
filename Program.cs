51e830d108015d4edf58d626604d7b526de0b860
Carlos Alfredo May Couoh <mastersgalaxi@gmail.com>
2024-05-09 10:36:39 -0600

    ....

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace MotorSport
         [STAThread]
         static void Main()
         {
+            //LML
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
